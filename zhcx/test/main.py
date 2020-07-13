772a5d9ffd15fb0caaff541e5bd7e0d7890b51d7
andychung <andywcchung@gmail.com>
2020-07-13 18:06:42 +0800

    .

--- zhcx/test/main.py
+++ zhcx/test/main.py
@@ -4,10 +4,14 @@ import nxr_frame
 
 can1 = can_comm.CanComm(lib_file='./ControlCAN.dll', can_dev=[0,1])
 
-print('Req_Addr')
-nxr_frame.req_addr(can1)
-print('='*20)
+#print('Req_Addr')
+#nxr_frame.req_addr(can1)
+#print('='*20)
 
 print('Req_Volt')
 nxr_frame.req_volt(can1, 0x01)
+print('='*20)
+
+print('Set_Volt')
+nxr_frame.set_volt(can1, 0x01, 100)
 print('='*20)
