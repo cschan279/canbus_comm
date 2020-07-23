949569a65e228ad0cc2618faefe1ee73d4201746
troy-x230 <troychan279@gmail.com>
2020-07-23 18:10:32 +0800

    .

--- zhcx/test/main.py
+++ zhcx/test/main.py
@@ -22,17 +22,57 @@ print('='*20)
 print('Set_Volt')
 nxr_frame.set_volt(can1, 0x01, 350)
 print('='*20)
+time.sleep(1)
+
+print('Req_Volt')
+nxr_frame.req_volt(can1, 0x01)
+print('='*20)
+'''
+print('Turn On')
+nxr_frame.turn_onoff(can1, 0x01, True)
+print('='*20)
+time.sleep(1)
+'''
+#input('Status is now ON, Press Enter to turn OFF')
+
+for i in range(5):
+    print('Turn On')
+    nxr_frame.turn_onoff(can1, 0x01, True)
+    print('='*20)
+    time.sleep(3)
+
+print('Set_Volt')
+nxr_frame.set_volt(can1, 0x01, 250)
+print('='*20)
+time.sleep(1)
+
+print('Req_Volt')
+nxr_frame.req_volt(can1, 0x01)
+print('='*20)
+
+
+for i in range(5):
+    print('Turn On')
+    nxr_frame.turn_onoff(can1, 0x01, True)
+    print('='*20)
+    time.sleep(3)
+
+print('Set_Volt')
+nxr_frame.set_volt(can1, 0x01, 300)
+print('='*20)
+time.sleep(1)
 
 
 
-input('Status is now ON, Press Enter to turn OFF')
 
-while True:
-	print('Turn On')
-	nxr_frame.turn_onoff(can1, 0x01, True)
-	print('='*20)
-	time.sleep(3)
-
-print('Status is now OFF')
+for i in range(5):
+    print('Req_Volt')
+    nxr_frame.req_volt(can1, 0x01)
+    print('='*20)
+    time.sleep(3)
 
 
+
+print('Turn Off')
+nxr_frame.turn_onoff(can1, 0x01, False)
+print('='*20)
