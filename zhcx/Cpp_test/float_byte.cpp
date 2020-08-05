2d2c94254bebdc6c3f355fea547537a65d63eed5
TroyC <troychan279@gmail.com>
2020-08-05 12:28:43 +0800

    .

--- zhcx/Cpp_test/float_byte.cpp
+++ zhcx/Cpp_test/float_byte.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 typedef unsigned char byte;
+struct floatarray {byte arr[4];}
+
+floatarray float2byte(float x){
+    floatarray y;
+    unsigned char const * p = reinterpret_cast<unsigned char const *>(&x);
+    for (int i =3; i >= 0; -- i){
+        y.arr[3-i] = (byte)p[i];
+    }
+    return y
+}
+
 
 int main()
 {
-    byte x[4];
-    float y = 1.2;
-    unsigned char const * p = reinterpret_cast<unsigned char const *>(&y);
-    for (int i =3; i >= 0; -- i){
+    floatarray y = float2byte(1.2);
+    byte z[4] = y.arr;
+    //byte x[4];
+    //float y = 1.2;
+    //unsigned char const * p = reinterpret_cast<unsigned char const *>(&y);
+    //for (int i =3; i >= 0; -- i){
     //int z = (int)y;
-        //cout<<hex<<(int)p[i]<< endl;
-        x[3-i] = (int)p[i];
-    }
-    for (int i=0; i<4;i++) cout << hex << (int)x[i] << endl;
+    //    //cout<<hex<<(int)p[i]<< endl;
+    //    x[3-i] = (int)p[i];
+    //}
+    for (int i=0; i<4;i++) cout << hex << (int)z[i] << endl;
     return 0;
 }
