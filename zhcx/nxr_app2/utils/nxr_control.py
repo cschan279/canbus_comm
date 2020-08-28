8bcd024a44e879cc83c90391f092887c01b63967
Troy Chan <troychan279@gmail.com>
2020-08-28 16:46:12 +0800

    .

--- zhcx/nxr_app2/utils/nxr_control.py
+++ zhcx/nxr_app2/utils/nxr_control.py
@@ -17,6 +17,11 @@ class NXR_CONTROL:
         self.loop_ret = True
         self.th = Thread(target=self.loop)
         self.th.start()
+
+        fid = nxr_conv.encode_id(ptp=False, dst=0xff, src=0xf0, grp=0x3)
+        fdt = nxr_conv.encode_data(func=0x10, rid=0x43, rdt=0, isfloat=False)
+        self.sendlist.append({"fid":fid, "fdt":fdt})
+
         return
 
     def loop(self):
@@ -31,6 +36,10 @@ class NXR_CONTROL:
         try:
             fid = self.sendlist[0]['fid']
             fdt = self.sendlist[0]['fdt']
+            print("x"*10, "Send", "x"*10)
+            nxr_conv.print_bin_id(fid)
+            nxr.conv.print_hex_ls(fdt)
+            print("x"*10, "Send", "x"*10)
             self.can_con.send(self.can_ch, fid, fdt)
             self.sendlist.pop(0)
         except Exception as e:
@@ -41,8 +50,13 @@ class NXR_CONTROL:
         try:
             fid, fdt = self.can_con.read(self.can_ch)
             if fid and fdt:
+                print("x"*10, "Read", "x"*10)
+                nxr_conv.print_bin_id(fid)
+                nxr.conv.print_hex_ls(fdt)
                 pro, ptp, dst, src, grp = nxr_conv.decode_id(fid)
                 err, rid, isfloat, rdt = nxr_conv.decode_data(fdt)
+                nxr.conv.print_hex_ls([grp, src, rid, rdt, err])
+                print("x"*10, "Read", "x"*10)
                 self.new_rec(grp, src, rid, rdt, err)
         except Exception as e:
             traceback.print_exc()
@@ -57,7 +71,7 @@ class NXR_CONTROL:
 
     def set(self, dst, grp, rid, rdt, isfloat):
         t = time.time()
-        fid = nxr_conv.encode_id(ptp=False, dst=dst, src=0xf0, grp=grp)
+        fid = nxr_conv.encode_id(ptp=True, dst=dst, src=0xf0, grp=grp)
         fdt = nxr_conv.encode_data(func=0x03, rid=rid, rdt=rdt,
                                    isfloat=isfloat)
         self.sendlist.append({"fid":fid, "fdt":fdt})
@@ -69,7 +83,7 @@ class NXR_CONTROL:
 
     def req(self, dst, grp, rid):
         t = time.time()
-        fid = nxr_conv.encode_id(ptp=False, dst=dst, src=0xf0, grp=grp)
+        fid = nxr_conv.encode_id(ptp=True, dst=dst, src=0xf0, grp=grp)
         fdt = nxr_conv.encode_data(func=0x10, rid=rid, rdt=0, isfloat=False)
         self.sendlist.append({"fid":fid, "fdt":fdt})
         for _ in range(20):
